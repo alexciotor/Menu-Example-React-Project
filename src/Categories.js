8a8075c82b0c1afef7fd7a8aae23ae1f6fd476bd
alexciotor <ciotor.alex@yahoo.com>
2020-12-15 12:27:53 +0200

    Finished Full Example Method

---
+++ src/Categories.js
@@ -0,0 +1,30 @@
+ import React from 'react';
+
+const Categories = ({filterItems,categories}) => {
+  
+  // Manual Button display Approach
+//   return(
+// <div className="btn-container">
+//  <button className="filter-btn" onClick={()=>filterItems('breakfast')} > breakfast</button>
+//   <button className="filter-btn" onClick={()=>filterItems('lunch')} > lunch</button>
+//    <button className="filter-btn" onClick={()=>filterItems('shakes')} > shakes</button>
+//     <button className="filter-btn" onClick={()=>filterItems('all')} > all</button>
+// </div>
+
+//   )
+
+// Dynamic Button display Approach
+
+return (
+<div className="btn-container">
+{categories.map((category,index)=>{
+  return <button type='button' className='filter-btn' key={index}  onClick={()=>{
+    filterItems(category)
+  }} >{category}</button>
+})}
+
+</div>
+);
+}
+
+export default Categories;
